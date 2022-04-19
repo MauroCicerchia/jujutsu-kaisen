0a69a8c192302b511df182fc6737aa3d95f99dfd
Mauro Cicerchia <maurocicerchia98@gmail.com>
2022-04-18 23:28:01 -0300

    feat: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import styles from "css/App.module.scss";
 import Navbar from "components/navbar";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.app}>
       <Navbar />
+      <Routes>
+        <Route element={<div>HOME</div>} path="/" />
+        <Route element={<div>CHARACTERS</div>} path="/characters" />
+      </Routes>
     </div>
   );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { memo } from "react";
+import { Link } from "react-router-dom";
 
-export default function Navbar() {
-  return <div>Navbar</div>;
-}
+export default memo(function Navbar() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/characters">Characters</Link>
+    </div>
+  );
+});
