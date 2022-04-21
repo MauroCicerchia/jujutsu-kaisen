2978a2f00d5c08d7e6c8a7b56c33f93256bac53c
Mauro Cicerchia <maurocicerchia98@gmail.com>
2022-04-20 22:12:49 -0300

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
-import styles from "css/App.module.scss";
-import Navbar from "components/navbar";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "components/navbar";
+import styles from "css/App.module.scss";
 
 function App() {
   return (
     <div className={styles.app}>
       <Navbar />
       <Routes>
-        <Route element={<div>HOME</div>} path="/" />
-        <Route element={<div>CHARACTERS</div>} path="/characters" />
+        <Route
+          element={
+            <h1>
+              Meet your favorite{" "}
+              <span className={styles.gradient}>Jujutsu Kaisen</span> characters
+            </h1>
+          }
+          path="/"
+        />
+        <Route
+          element={
+            <h1>
+              <span className={styles.gradient}>Characters</span>
+            </h1>
+          }
+          path="/characters"
+        />
       </Routes>
     </div>
   );
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,16 @@
 import React, { memo } from "react";
 import { Link } from "react-router-dom";
+import styles from "./Navbar.module.scss";
 
 export default memo(function Navbar() {
   return (
     <div>
-      <Link to="/">Home</Link>
-      <Link to="/characters">Characters</Link>
+      <Link className={styles.link} to="/">
+        Home
+      </Link>
+      <Link className={styles.link} to="/characters">
+        Characters
+      </Link>
     </div>
   );
 });
